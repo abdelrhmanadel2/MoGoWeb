9940e781b3bace2db01c65a161197f9c13f8d572
abdelrhmanadel2 <47029136+abdelrhmanadel2@users.noreply.github.com>
2021-03-13 06:45:00 +0200

    nan-links scroll edit

--- js/script.js
+++ js/script.js
@@ -36,60 +36,33 @@ $(document).ready(function () {
 
   });
   $('.loader').fadeOut(2000);
-  if($(window).scrollTop() < (aboutoff-30)){
-    $("nav ul li.active").removeClass('active');
-  
-    $(`nav ul li.home`).addClass('active');
-  }
+
 });
 
 
 let aboutoff = $('#about-us').offset().top;
 
-//add active class to navlink
-$('.nav-item a').on('click',function (e) {
+//scroll to sections
+$('.nav-item a').on('click', function (e) {
   e.preventDefault();
   var target = this.hash,
-  $target = $(target);
+    $target = $(target);
 
   $('html, body').stop().animate({
-      'scrollTop': $target.offset().top
+    'scrollTop': $target.offset().top
   }, 500, 'swing', function () {
-      window.location.hash = target;
+    window.location.hash = target;
   });
 });
-
-  
+//scrolltopbutton 
 var toggleScrollToTopButton = function () {
-  
-  if ( $(window).scrollTop() > 1000) {
-        $('.scrolltotop ').fadeIn();
-    } else {
-        $('.scrolltotop ').fadeOut();
-    } 
+
+  if ($(window).scrollTop() > 1000) {
+    $('.scrolltotop ').fadeIn();
+  } else {
+    $('.scrolltotop ').fadeOut();
+  }
 }
-var addClassOnScroll = function () {
-  
-  $('header,section ,footer').each(function (i, elem) {
-
-      var offsetTop = $(elem).offset().top;  
-
-      if(  $(window).scrollTop() > (parseInt (offsetTop)-250) &&  $(window).scrollTop() < ( offsetTop)) {
-
-          var elemId = $(elem).attr('id')
-
-          $("nav ul li.active").removeClass('active');
-
-          $(`nav ul li.${elemId}`).addClass('active');
-
-      }
-    
-      
-  });
-  
-};
-
-//scrolltopbutton
 $('.scrolltotop ,.navbar-brand ').each(function () {
   $(this).click(function () {
     $('html,body').animate({ scrollTop: 0 }, 'slow');
@@ -97,30 +70,17 @@ $('.scrolltotop ,.navbar-brand ').each(function () {
   });
 });
 
-$(window).on('scroll',function(){
- 
+$(window).on('scroll', function () {
   toggleScrollToTopButton();
   togglenavcolor();
-  addClassOnScroll();
-  if($(window).scrollTop() < (aboutoff-30)){
-    $("nav ul li.active").removeClass('active');
-  
-    $(`nav ul li.home`).addClass('active');
-  }
 })
 
 // add color to navbar
-var togglenavcolor=function () {
-  let windowscroll = $(window).scrollTop();
-
-  if ( $(window).scrollTop() > 0)  {
+var togglenavcolor = function () {
+  if ($(window).scrollTop() > 0) {
     $('#navbar').addClass('navcolor');
-  
-
   }
   else {
     $('#navbar').removeClass('navcolor');
   }
-  
-
 }
